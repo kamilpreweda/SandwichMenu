c1604c9eade79b7583a5032b459863677916f79f
Kamil Preweda <preweda.kamil@o2.pl>
2022-05-21 16:26:04 +0200

    Added "Generate" method

--- SandwichMenu/MenuItem.cs
+++ SandwichMenu/MenuItem.cs
@@ -17,5 +17,20 @@ namespace SandwichMenu
         string Description = "";
         string Price;
 
+
+
+        public void Generate()
+        {
+            string randomProtein = Proteins[Randomizer.Next(Proteins.Length)];
+            string randomCondiment = Condiments[Randomizer.Next(Condiments.Length)];
+            string randomBread = Breads[Randomizer.Next(Breads.Length)];
+            Description = $"{randomProtein} i {randomCondiment}. Pieczywo: {randomBread}";
+
+            decimal bucks = Randomizer.Next(2, 5);
+            decimal cents = Randomizer.Next(1, 98);
+            decimal price = bucks + (cents * 0.1M);
+            Price = price.ToString("c");
+
+
     }
-}
+    } }
