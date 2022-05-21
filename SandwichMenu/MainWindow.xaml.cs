e8729f5bff51be3c6ca73f5821595afbcf407510
Kamil Preweda <preweda.kamil@o2.pl>
2022-05-21 17:20:05 +0200

    Added "MakeTheMenu" method

--- SandwichMenu/MainWindow.xaml.cs
+++ SandwichMenu/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using SandwichMenu;
 
 namespace SandwichMenu
 {
@@ -23,6 +24,51 @@ namespace SandwichMenu
         public MainWindow()
         {
             InitializeComponent();
+            MakeTheMenu();
+        }
+        private void MakeTheMenu()
+        {
+            MenuItem[] menuItems = new MenuItem[5];
+            string guacamolePrice;
+
+            for (int i = 0; i < 5; i++)
+            {
+                menuItems[i] = new MenuItem();
+                if (i <= 3)
+                {
+                    menuItems[i].Breads = new string[] { "zwykły rogal", "rogal cebulowy", "rogal z pumpernikla", "rogal z wszystkim" };
+                };
+
+                menuItems[i].Generate();
+            }
+
+            item1.Text = menuItems[0].Description;
+            price1.Text = menuItems[0].Price;
+            item2.Text = menuItems[1].Description;
+            price2.Text = menuItems[1].Price;
+            item3.Text = menuItems[2].Description;
+            price3.Text = menuItems[2].Price;
+            item4.Text = menuItems[3].Description;
+            price4.Text = menuItems[3].Price;
+            item5.Text = menuItems[4].Description;
+            price5.Text = menuItems[4].Price;
+
+            MenuItem specialMenuitem = new MenuItem()
+            {
+                Proteins = new string[] { "Szynka organiczna", "Burger z grzybów", "Mortadella"},
+                Breads = new string[] { "bułka bezglutenowa", "tortilla", "pita" },
+                Condiments = new string[] { "musztarda diżońska", "sos z miso", "au jus" }
+            };
+            specialMenuitem.Generate();
+
+            item6.Text = specialMenuitem.Description;
+            price6.Text = specialMenuitem.Price;
+
+            MenuItem guacamoleMenuItem = new MenuItem();
+            guacamoleMenuItem.Generate();
+            guacamolePrice = guacamoleMenuItem.Price;
+
+            guacamole.Text = $"Dodaj guacamole za {guacamolePrice}";
         }
     }
 }
--- SandwichMenu/MenuItem.cs
+++ SandwichMenu/MenuItem.cs
@@ -8,14 +8,14 @@ namespace SandwichMenu
 {
     public class MenuItem
     {
-        Random Randomizer = new Random();
+        public Random Randomizer = new Random();
 
-        string[] Proteins = { "Wołowina", "Salami", "Indyk", "Szynka", "Pastrami", "Tofu" };
-        string[] Condiments = { "musztarda żółta", "musztarda brązowa", "musztarda miodowa", "majonez", "przyprawy", "sos francuski" };
-        string[] Breads = { "ryżowe", "białe", "pszenne", "pumpernikiel", "bułka" };
+        public string[] Proteins = { "Wołowina", "Salami", "Indyk", "Szynka", "Pastrami", "Tofu" };
+        public string[] Condiments = { "musztarda żółta", "musztarda brązowa", "musztarda miodowa", "majonez", "przyprawy", "sos francuski" };
+        public string[] Breads = { "ryżowe", "białe", "pszenne", "pumpernikiel", "bułka" };
 
-        string Description = "";
-        string Price;
+        public string Description = "";
+        public string Price;
 
 
 
