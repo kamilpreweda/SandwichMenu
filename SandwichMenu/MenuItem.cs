bae7af67d88c1c35fc1270a7db3da3a4fb5a77d5
Kamil Preweda <preweda.kamil@o2.pl>
2022-05-21 16:18:26 +0200

    Dodaj pliki projektów.

---
+++ SandwichMenu/MenuItem.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SandwichMenu
+{
+    public class MenuItem
+    {
+        Random Randomizer = new Random();
+
+        string[] Proteins = { "Wołowina", "Salami", "Indyk", "Szynka", "Pastrami", "Tofu" };
+        string[] Condiments = { "musztarda żółta", "musztarda brązowa", "musztarda miodowa", "majonez", "przyprawy", "sos francuski" };
+        string[] Breads = { "ryżowe", "białe", "pszenne", "pumpernikiel", "bułka" };
+
+        string Description = "";
+        string Price;
+
+    }
+}
